1b4ade08e0c490bf5d941acc06e460da377f2973
feiin <feiin@qq.com>
2016-02-04 11:25:10 +0800

    add html tempalte

---
+++ swiftmi/swiftmi/HtmlPlayer/next.js
@@ -0,0 +1,52 @@
+/// <reference path="jquery-1.8.3.min.js" />
+/// <reference path="tmpl.js" />
+;
+(function () {
+
+ moment.lang("zh-cn");
+ template.helper('Date', Date);
+ template.helper('moment', moment);
+
+    var article = {
+        isNight: 0,
+        render: function (art) {
+
+            if (!art.article.createDate) {
+                art.article.createDate = parseInt((new Date()).getTime() / 1000);
+            }
+
+            var con = template("content-tmpl", art);
+            $("#content").html(con);
+
+            if($(".highlight").length == 0) {
+                hljs.initHighlighting();
+            }
+            window.location.href = "html://contentready";
+
+            return 1;
+        },
+
+        setFontSize: function (sizeName) {
+
+            $("body")[0].className = "body f" + sizeName;
+            article.setNightMode(article.isNight);
+            return 1;
+        },
+
+        setNightMode: function (isNight) {
+            article.isNight = isNight;
+            if (isNight) {
+                $("body").addClass("night");
+            }
+            else {
+                $("body").removeClass("night");
+            }
+            return 1;
+        }
+    };
+
+    window.article = article;
+    $(document).ready(function () {
+        window.location.href = "html://docready";
+    });
+})();
